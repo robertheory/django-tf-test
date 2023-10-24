d782db967c248c1350ea5ce02ced2374cd3567e2
Roberto Costa <roberto.araujo339@gmail.com>
2023-10-24 16:53:57 -0300

    project setup

--- main.tf
+++ main.tf
@@ -18,12 +18,6 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0e83be366243f524a"
   instance_type = "t2.micro"
   key_name = "id_rsa"
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt-get update
-              sudo apt-get install -y nginx
-              sudo systemctl start nginx
-              EOF
 
   tags = {
     Name = "test-terraform"
